610b33507c011270d949f4357d109e7ac809ad58
Timur Berezov <berezov.timur@gmail.com>
2020-04-30 14:45:42 +0300

    Add files

---
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -0,0 +1,71 @@
+package ru.netology.domain;
+
+public class Conditioner {
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+    private boolean on;
+
+   /* public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }*/
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+        if (currentTemperature > maxTemperature) {
+            return;
+        }
+        if (currentTemperature < minTemperature) {
+            return;
+        }
+        this.currentTemperature = currentTemperature;
+    }
+
+    public void increaseCurrentTemperature() {
+        int increasedTemperature = currentTemperature + 1;
+        if (increasedTemperature > maxTemperature) {
+            return;
+        }
+        this.currentTemperature = increasedTemperature;
+    }
+
+    public void decreaseCurrentTemperature(){
+        int decreasedTemperature = currentTemperature - 1;
+        if (decreasedTemperature < minTemperature) {
+            return;
+        }
+        this.currentTemperature = decreasedTemperature;
+    }
+
+/*    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }*/
+}
---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,59 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+
+    /*@Test
+    public void shouldNotChangeFieldValue(){
+        Conditioner conditioner = new Conditioner();
+        assertEquals(0, conditioner.getCurrentTemperature());
+        conditioner.setCurrentTemperature(100);
+        assertEquals(0, conditioner.getCurrentTemperature());
+
+        System.out.println("Hi");
+    }*/
+
+    @Test
+    public void shouldIncreaseTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(35);
+        conditioner.setMinTemperature(14);
+        conditioner.setCurrentTemperature(23);
+        conditioner.increaseCurrentTemperature();
+        assertEquals(24, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldIncreaseOverMaxTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(35);
+        conditioner.setMinTemperature(14);
+        conditioner.setCurrentTemperature(35);
+        conditioner.increaseCurrentTemperature();
+        assertEquals(35, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldDecreaseTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(35);
+        conditioner.setMinTemperature(14);
+        conditioner.setCurrentTemperature(23);
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(22, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldDecreaseUnderMinTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(35);
+        conditioner.setMinTemperature(14);
+        conditioner.setCurrentTemperature(14);
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(14, conditioner.getCurrentTemperature());
+    }
+
+    }
